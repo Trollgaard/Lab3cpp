2335bb1a3c4f5233bacde3f8c11dfdc425a602ab
Dirionz <chrillesmash@hotmail.com>
2014-03-24 10:03:06 +0100

    added dec

--- Header/Cirkel.h
+++ Header/Cirkel.h
@@ -1,3 +1,4 @@
+//Detta är en cirkel som tillhör en figur i den fin soppa på facebook, alltså digitalsoppa!
 class Cirkel : public Figur
 {public:
 	Cirkel(Canvas & cv, int width, int height, Pointf cp, Colorf cf);
